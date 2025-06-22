5e5955f8e836612f7b9a87f9f18ab7b1209aa344
Jonas Wortmann <jowort04@gmail.com>
2025-06-22 14:21:38 +0200

    right energies, better setup

--- src/multiple_scattering.py
+++ src/multiple_scattering.py
@@ -4,7 +4,7 @@ import numpy as np
 Z = np.array([29., 13.])
 
 m = .511 # GeV
-E = 3.5 # GeV (elsa max energy)
+E = 2.9 # GeV (elsa max energy)
 p = np.sqrt(E**2 - m**2) # GeV 
 
 beta_c = 1. # v = 1
@@ -14,17 +14,20 @@ x0_rho = np.array([12.86, 24.01]) # g/cm²
 x0 = np.array([1.436, 8.897]) # cm
 
 # thickness of material
-x = np.array([1, 18]) # cm
+x = np.array([.07, .7]) # cm
 
 theta = np.ndarray(shape=(2,))
 first_sigma = np.ndarray(shape=(2,))
 second_sigma = np.ndarray(shape=(2,))
 third_sigma = np.ndarray(shape=(2,))
 
+material = ["Copper", "Aluminium"]
+
 # formula for multiple scattering
 # first Cu second Al
 for i in range(2):
     theta[i] = .0306 * 1./beta_c * 1./p * Z[i] * np.sqrt(x[i] / x0[i]) * (1. + 0.038 * np.log(x[i] / x0[i]))
+    print(material[i], x[i], "[cm]")
     print("angle: ", theta[i]*180./np.pi, "[degree]")
     # 10 cm², roughly 4x4 cm² for error
     first_sigma[i] = 4./np.tan(1. * theta[i])
@@ -36,5 +39,4 @@ for i in range(2):
     print("----------")
 
 # questions:
-# what's the max energy of the electrons? 3.5 GeV?
 # what's the max distance between the material and the detector which can be achieved at the setup?
