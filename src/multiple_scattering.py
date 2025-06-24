c3634b9349d5085aaf09e7e89be644ba84e9cceb
Jonas Wortmann <jowort04@gmail.com>
2025-06-24 18:42:16 +0200

    consistency in rad degree

--- src/multiple_scattering.py
+++ src/multiple_scattering.py
@@ -20,7 +20,7 @@ x0_rho = np.array([12.86, 24.01]) # g/cm²
 x0 = np.array([1.436, 8.897]) # cm
 
 # thickness of material
-x = np.array([10, 7000]) # cm
+x = np.array([.04, .4]) # cm
 
 theta = np.ndarray(shape=(2,))
 first_sigma = np.ndarray(shape=(2,))
@@ -33,12 +33,18 @@ material = ["Copper", "Aluminium"]
 # first Cu second Al
 for i in range(2):
     theta[i] = .0306 * 1./beta_c * 1./p * Z[i] * np.sqrt(x[i] / x0[i]) * (1. + 0.038 * np.log(x[i] / x0[i]))
+
     print(material[i], x[i], "[cm]")
-    print("angle: ", theta[i], "[degree]")
+
+    # the unit of the angle is still unsure. the unit might from the formula might be rad and not degree
+    print("angle: ", theta[i], "[rad]")
+    print("angle: ", theta[i]*180/np.pi, "[degree]")
+
     # 10 cm², roughly 4x4 cm² for error
-    first_sigma[i] = 4./np.tan(1. * theta[i]*np.pi/180)
-    second_sigma[i] = 4./np.tan(2. * theta[i]*np.pi/180)
-    third_sigma[i] = 4./np.tan(3. * theta[i]*np.pi/180)
+    # np.tan takes rad as argument. if theta is rad then this works
+    first_sigma[i] = 4./np.tan(1. * theta[i])
+    second_sigma[i] = 4./np.tan(2. * theta[i])
+    third_sigma[i] = 4./np.tan(3. * theta[i])
     print("first sigma: ", first_sigma[i], "[cm]")
     print("second sigma: ", second_sigma[i], "[cm]")
     print("third sigma: ", third_sigma[i], "[cm]")
