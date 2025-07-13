4d3b10a340b12cdccb0abfa661942f6d4d3e7c49
Florian <Florian@Bierlage.de>
2025-07-13 16:25:05 +0200

    blablabla

--- src/elsa/multiple_scattering.py
+++ src/elsa/multiple_scattering.py
@@ -14,10 +14,10 @@ x0_rho = np.array([12.86, 24.01]) # g/cm²
 x0 = np.array([1.436, 8.897]) # cm
 
 # thickness of material
-x = np.array([.2, 5]) # cm
+x = np.array([2.96, 18.65]) # cm
 
 # distance target detector
-d = 15 # cm
+d = 40 # cm
 #print("distance: ", d, "[cm]")
 
 theta = np.ndarray(shape=(2,))
@@ -27,14 +27,45 @@ material = ["Copper", "Aluminium"]
 
 # formula for multiple scattering
 # first Cu second Al
-for i in range(2):
-    theta[i] = .0136 * 1./beta_c * 1./p * Z[i] * np.sqrt(x[i] / x0[i]) * (1. + 0.038 * np.log(x[i] / x0[i]))
 
-    print(material[i], x[i], "[cm]")
+def multipleScattering(thickness, radLength, Z):
+    return .0136 * (1./beta_c) * (1./p) * 1 * np.sqrt((thickness/radLength)) * (1.0 + 0.038 * np.log(thickness/radLength))
 
-    print("angle: ", theta[i], "[rad]")
-    print("angle: ", theta[i]*180/np.pi, "[degree]")
+thicknessArray = np.array([
+        5, 9.93, 21, 27.39,
+        0.7, 1.48, 2.96, 4.36
+        ])
+materialArray = np.array([
+        1,1,1,1,
+        0,0,0,0
+        ])
 
+for i in range(0,8):
+    theta = multipleScattering(
+            thicknessArray[i], 
+            x0[materialArray[i]], 
+            Z[materialArray[i]]
+            )
+    print(material[materialArray[i]], thicknessArray[i], "[cm]")
+
+    print("angle: ", theta, "[rad]")
+    print("angle: ", theta*180/np.pi, "[degree]")
+    
+    print("-------------------------")
+
+
+
+
+
+
+#for i in range(2):
+#    theta[i] = .0136 * 1./beta_c * 1./p * Z[i] * np.sqrt(x[i] / x0[i]) * (1. + 0.038 * np.log(x[i] / x0[i]))
+#
+#    print(material[i], x[i], "[cm]")
+#
+#    print("angle: ", theta[i], "[rad]")
+#    print("angle: ", theta[i]*180/np.pi, "[degree]")
+#
     #print("adjacent side length: ", np.tan(theta[i]) * d, "[cm]")
           
     # 10 cm², roughly 4x4 cm² for error
@@ -42,7 +73,19 @@ for i in range(2):
     #distance_target_detector[i] = 2./np.tan(3. * theta[i])
     #print("distance target-detector (theory): ", distance_target_detector[i], "[cm]")
 
-    print("----------")
+#    print("----------")
+###
+
+
+
+
+
+
+
+
+
+
+
 
 ''' testing
 # cummulated
